baa5a50fccfdeb65b654cce9de7590dd3d6e88e9
Davy Piratao <davidantoniopetro@gmail.com>
2025-09-22 21:56:50 -0300

    Update: calculo da DivisaoComResto separada em 2 funcões.

--- DivisaoComResto.java
+++ DivisaoComResto.java
@@ -2,9 +2,15 @@ public class DivisaoComResto {
     public static void main(String[] args) {
         int totaldeItens = 17;
         int capacidadedaCaixa = 5;
-        int numeroDeCaixas = totaldeItens / capacidadedaCaixa;
-        int itensRestantes = totaldeItens % capacidadedaCaixa;
+        int numeroDeCaixas = calcularNumeroDeCaixas(totaldeItens, capacidadedaCaixa);
+        int itensRestantes = calcularItensRestantes(totaldeItens, capacidadedaCaixa);
         System.out.println("Serão preenchidas " + numeroDeCaixas + " caixas.");
         System.out.println("Sobrarão " + itensRestantes + " itens.");
+    }
+    public static int calcularNumeroDeCaixas(int totaldeItens, int capacidadedaCaixa) {
+        return totaldeItens / capacidadedaCaixa;
+    }
+    public static int calcularItensRestantes(int totaldeItens, int capacidadedaCaixa) {
+        return totaldeItens % capacidadedaCaixa;
     }
 }
