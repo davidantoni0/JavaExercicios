cf19f28f9ab3c18a3edee4f6670f72f6d5e12294
David Antônio Gonçalves <pet0104207@aluno.firjansenaisesi.com.br>
2025-09-24 19:15:12 -0300

    Update: swich-case changed to ternary operation on IdentificadorDeSemestre file.

--- IdentificadorDeSemestre.java
+++ IdentificadorDeSemestre.java
@@ -1,17 +1,9 @@
 public class IdentificadorDeSemestre {
     public static void main(String[] args) {
-        int numeroDoMes = 8;
+        int numeroDoMes = 3;
 
-        switch (numeroDoMes) {
-            case 1, 2, 3, 4, 5, 6:
-                System.out.println("Primeiro semestre");
-                break;
-            case 7, 8, 9, 10, 11, 12:
-                System.out.println("Segundo semestre");
-                break;
-            default:
-                System.out.println("Mês inválido");
-                break;
-        }
+        System.out.println(numeroDoMes >= 1 && numeroDoMes <= 6 ? "Primeiro semestre" : 
+                           numeroDoMes >= 7 && numeroDoMes <= 12 ? "Segundo semestre" : 
+                           "Mês inválido");
     }
 }
