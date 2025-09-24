c7eb435a37740e141cf2ae211588e38f4dc6ddb5
David Antônio Gonçalves <pet0104207@aluno.firjansenaisesi.com.br>
2025-09-24 20:28:09 -0300

    Fix: Made the code on CassificadorDeIdade simpler.

--- ClassificadorDeIdade.java
+++ ClassificadorDeIdade.java
@@ -1,18 +1,19 @@
 public class ClassificadorDeIdade {
     public static void main(String[] args) {
-        int idade = 25;
+        int idade = -1;
 
         do {
+
             if (idade < 0) {
                 System.out.println("Idade inválida");
             }
             else if(idade <= 12){
                 System.out.println("Criança");
             }
-            else if(idade >= 13 && idade <= 19){
+            else if(idade <= 19){
                 System.out.println("Adolescente");
             }
-            else if(idade >= 20 && idade <= 59){
+            else if(idade <= 59){
                 System.out.println("Adulto");
             }
             else{
--- IdentificadorDeSemestre.java
+++ IdentificadorDeSemestre.java
@@ -1,6 +1,6 @@
 public class IdentificadorDeSemestre {
     public static void main(String[] args) {
-        int numeroDoMes = 3;
+        int numeroDoMes = 8;
 
         System.out.println(numeroDoMes >= 1 && numeroDoMes <= 6 ? "Primeiro semestre" : 
                            numeroDoMes >= 7 && numeroDoMes <= 12 ? "Segundo semestre" : 
