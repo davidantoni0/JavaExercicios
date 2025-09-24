77b9c46ca583abb5f42372d9ac9d8de16ab9c11f
David Antônio Gonçalves <pet0104207@aluno.firjansenaisesi.com.br>
2025-09-24 18:41:12 -0300

    Update: desconto adicionado a CalculadoraSalario file.

--- CalculadoraSalario.java
+++ CalculadoraSalario.java
@@ -1,19 +1,25 @@
 public class CalculadoraSalario {
     public static void main(String[] args) {
         String nome = "João";
-        double salario = 2500.0;
+        double salario = 1600.0;
+        double desconto;
 
         if(salario <= 1751.81){
-            System.out.println("O funcionário " + nome + " tem um salário de R$ " + salario + " e terá descontado 8% da sua renda.");
+            desconto = salario * 0.08;
+
+            System.out.println("O funcionário " + nome + " tem um salário de R$ " + salario + " e terá descontado " +desconto+ " da sua renda, ficando com um salário final de R$ " + (salario - desconto));
         }
         else if(salario >= 1751.82 && salario <= 2919.72){
-            System.out.println("O funcionário " + nome + " tem um salário de R$ " + salario + " e terá descontado 9% da sua renda.");
+            desconto = salario * 0.09;
+            System.out.println("O funcionário " + nome + " tem um salário de R$ " + salario + " e terá descontado " +desconto+ " da sua renda, ficando com um salário final de R$ " + (salario - desconto));
         }
         else if(salario >= 2919.73 && salario <= 5839.45){
-            System.out.println("O funcionário " + nome + " tem um salário de R$ " + salario + " e terá descontado 10% da sua renda.");
+            desconto = salario * 0.10;
+            System.out.println("O funcionário " + nome + " tem um salário de R$ " + salario + " e terá descontado " +desconto+ " da sua renda, ficando com um salário final de R$ " + (salario - desconto));
         }
         else{
-            System.out.println("O funcionário " + nome + " tem um salário de R$ " + salario + " e terá descontado 11% da sua renda.");
+            desconto = salario * 0.11;
+            System.out.println("O funcionário " + nome + " tem um salário de R$ " + salario + " e terá descontado " +desconto+ " da sua renda, ficando com um salário final de R$ " + (salario - desconto));
         }
     }
 }
