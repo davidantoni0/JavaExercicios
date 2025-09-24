870d8c34ca5b63419914e0311a6b06ce6c392dae
David Antônio Gonçalves <pet0104207@aluno.firjansenaisesi.com.br>
2025-09-23 21:05:40 -0300

    Add: SituacaoAluno.java file

---
+++ SituacaoAluno.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class SituacaoAluno {
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        System.out.println("Digite a primeira nota: ");
+        double nota1 = scanner.nextDouble();
+        System.out.println("Digite a segunda nota: ");
+        double nota2 = scanner.nextDouble();
+        double media = (nota1 + nota2) / 2;
+        
+        if (media == 10) {
+            System.out.println("Aprovado parabéns!");  
+        }else if (media >= 7.0) {
+            System.out.println("Aprovado!");
+        } else {
+            System.out.println("Reprovado");
+        }
+        scanner.close();
+    }
+    
+}
