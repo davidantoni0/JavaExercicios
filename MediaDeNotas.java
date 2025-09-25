99911f17437ca7adc30da5f4660976055fd85e83
David Antônio Gonçalves <pet0104207@aluno.firjansenaisesi.com.br>
2025-09-25 18:24:47 -0300

    Update: MediaDeNotas variable values(notas).

--- MediaDeNotas.java
+++ MediaDeNotas.java
@@ -2,7 +2,7 @@ public class MediaDeNotas {
     public static void main(String[] args) {
         double soma = 0;
         final int TOTALNOTAS = 5;
-        double[] notas = {5.0, -8.0, 5.0, 12.0, 5.0};
+        double[] notas = {7.5, -8.0, 6.5, 12.0, 5.5};
         for(int i = 0; i < TOTALNOTAS; i++){
             if(notas[i] >= 0 && notas[i] <= 10){
                 soma += notas[i];
