95771c4d5f4e7a3f96c8d9895aeaf640eadf439b
David Antônio Gonçalves <pet0104207@aluno.firjansenaisesi.com.br>
2025-09-25 20:46:33 -0300

    Update: loop for on MediaNotas.

--- MediaDeNotas.java
+++ MediaDeNotas.java
@@ -1,15 +1,14 @@
 public class MediaDeNotas {
     public static void main(String[] args) {
         double soma = 0;
-        final int TOTALNOTAS = 5;
         double[] notas = {7.5, -8.0, 6.5, 12.0, 5.5};
-        for(int i = 0; i < TOTALNOTAS; i++){
-            if(notas[i] >= 0 && notas[i] <= 10){
-                soma += notas[i];
+        for(double nota : notas){
+            if(nota >= 0 && nota <= 10){
+                soma += nota;
             } else {
                 continue;
             }
         }
-        System.out.println("A média das notas válidas é: " + (soma / TOTALNOTAS));
+        System.out.println("A média das notas válidas é: " + (soma / notas.length));
     }
 }
