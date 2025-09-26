25a3523bb11c9b7b230cc1886063e566a4195870
David Antônio Gonçalves <pet0104207@aluno.firjansenaisesi.com.br>
2025-09-25 21:32:55 -0300

    Update: MaiorNumero variable value set to 0.

--- MaiorNumero.java
+++ MaiorNumero.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class MaiorNumero {
     public static void main(String[] args) {
         int[] numeros = new int[5];
-        int maiorNumero = Integer.MIN_VALUE;
+        int maiorNumero = 0;
         Scanner scanner = new Scanner(System.in);
 
         for(int i = 0; i < numeros.length; i++){
