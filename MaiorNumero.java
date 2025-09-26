0684546057c1ae23e9a42346202daaea3847604f
David Antônio Gonçalves <pet0104207@aluno.firjansenaisesi.com.br>
2025-09-25 21:25:15 -0300

    Add: MaiorNumero file.

---
+++ MaiorNumero.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MaiorNumero {
+    public static void main(String[] args) {
+        int[] numeros = new int[5];
+        int maiorNumero = Integer.MIN_VALUE;
+        Scanner scanner = new Scanner(System.in);
+
+        for(int i = 0; i < numeros.length; i++){
+            System.out.println("Digite o número " + (i + 1) + ":");
+            numeros[i] = scanner.nextInt();
+            if(numeros[i] > maiorNumero){
+                maiorNumero = numeros[i];
+            }
+        }
+        System.out.println("O maior número é: " + maiorNumero);
+        scanner.close();
+    }
+}
